b0573bfab68b3963a9c877e9c0080d6ecf0cce85
86chan <47755106+86chan@users.noreply.github.com>
2026-03-20 01:23:14 +0900

    chore: 依存関係の更新および Java 21 へのアップグレード

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 
 android {
     namespace = "com.haru.ocrkeyboard"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.haru.ocrkeyboard"
@@ -32,12 +28,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
     }
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
